1413c687c17cc9c1c57fa8ab5d2d720b4260fd06
wtdavis <wtdavis@wesleyan.edu>
2023-02-22 17:27:46 -0500

    views started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+        render :show
+    end
+
     private
+    
     def user_params
         params.require(:user).permit(:username, :password)
     end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
     it "works! (now write some real specs)" do
-      get users_path
+      user1 = User.new(username: "Will", password: "password")
+      user1.save!
+      get users_url
       expect(response).to have_http_status(200)
     end
   end
